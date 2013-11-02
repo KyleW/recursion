e7010f5503a849084063b34963a47b8d0dbc95f3
Kyle Warneck <Kyle.Warneck@gmail.com>
2013-11-02 14:18:15 -0700

    All but one test pass for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,40 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+    // your code goes here
+  	debugger;
+	var result = "";	
+	var what = typeof(obj);
+	if (what === 'string'){
+		result += '"' + obj + '"';
+	} else if (what === 'number'){
+		result += obj.toString();
+	} else if (what === 'boolean'){
+		result += obj.toString();
+	} else if (Array.isArray(obj)) {
+		result += "[";
+		for (var i = 0; i < obj.length; i++){
+			if (i>0){
+				result += ",";
+			}
+			result += stringifyJSON(obj[i]);
+		}
+		result +="]"
+	} else if (obj === null){
+		result += null;
+	} else {
+		result += "{";
+		var counter = 0;
+		for (var key in obj){
+			if (obj[key] !=== undefined) {
+				if (counter>0){
+					result +=",";
+				}
+				result += stringifyJSON(key) + ":" +stringifyJSON(obj[key]);
+				counter++;
+				}
+			}
+		result += "}";
+	}
+	return result;
 };
