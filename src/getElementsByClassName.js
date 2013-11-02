239e1c43c022ab744d9dded23915ee44bd546cbe
Kyle Warneck <Kyle.Warneck@gmail.com>
2013-11-02 15:13:27 -0700

    All tests passing for getElementsByClassName except for 1

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,21 @@
 // };
 
 // But in stead we're going to implement it from scratch:
+
 var getElementsByClassName = function (className) {
-  // your code here
+	var result = [];
+	if (document.body.hasChildNodes()){
+		var children =document.body.childNodes;
+	}
+	
+	for ( var i = 0; i < children.length; i++){
+		if (children[i].classList){
+			for(var key in children[i].classList){
+				if (children[i].classList[key] === className){
+					result.push(children[i]);
+				}
+			}
+		}
+	}
+	return result;
 };
